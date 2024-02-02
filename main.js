358cc1ee611960a000e6246936f877b9d7cdde30
David Engelen <robelene112@gmail.com>
2024-02-02 21:47:20 +0100

    Finish header on both mobile and desktop.

--- main.js
+++ main.js
@@ -1,24 +1,16 @@
 const hamburgerMenu = document.querySelector('.hamburger-menu')
 const header = document.querySelector('header')
 const headerContainer = document.querySelector('.header-container')
-const mobileNavContainer = document.querySelector('.mobile-nav-container')
+const navMenuContainer = document.querySelector('.nav-menu-container')
+const navContainer = document.querySelector('.nav-container')
 const mobileNav = document.querySelector('.mobile-nav')
 const desktopNav = document.querySelector('nav')
 
 document.querySelector('.hamburger-menu').addEventListener('click', () => {
 	hamburgerMenu.classList.toggle('active')
 	header.classList.toggle('active')
-	mobileNavContainer.classList.toggle('active')
+	navMenuContainer.classList.toggle('active')
+	navContainer.classList.toggle('active')
 	mobileNav.classList.toggle('active')
-
-	mobileNav.appendChild(desktopNav)
-
-	mobileNav.firstChild.classList.toggle('mobile-nav-items')
 })
 
-window.onresize = () => {
-	if (window.innerWidth > 1000) {
-		headerContainer.appendChild(mobileNav.firstChild)
-		mobileNav.removeChild(mobileNav.firstChild)
-	}
-}
